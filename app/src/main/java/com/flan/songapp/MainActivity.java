083471abb43250970ab3222847dee83f640603df
Fanny Zhou <fzhou9@illinois.edu>
2018-04-27 12:40:38 -0500

    implementting movie database

--- app/src/main/java/com/flan/songapp/MainActivity.java
+++ app/src/main/java/com/flan/songapp/MainActivity.java
@@ -1,15 +1,62 @@
 package com.flan.songapp;
 
 
+import android.app.DownloadManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class MainActivity extends AppCompatActivity {
     // This is a test comment 4/23/18 11:30 am
+    private static RequestQueue requestQueue;
+
+    private static final String TAG = "SongApp:Main";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set up the queue for our API requests
+        requestQueue = Volley.newRequestQueue(this);
         setContentView(R.layout.activity_main);
+        startAPICall();
     }
+    void startAPICall() {
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    "https://api.themoviedb.org/3/movie/550?api_key="
+                            + "bbec13ad5f0b916db99eef893310f76c",
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            try {
+                                Log.d(TAG, response.toString(2));
+                            } catch (JSONException ignored) { }
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    Log.e(TAG, error.toString());
+                }
+            });
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
+
